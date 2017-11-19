c6148eb941cb865cd090bf17cd34aa8a8b899335
matiasmacaya <keny92@gmail.com>
2017-11-19 15:55:33 -0300

    falta responder preguntas del gugl doc, y copmilar ejecutable

--- PAC.py
+++ PAC.py
@@ -6,34 +6,32 @@
 #   NOMINA DE COBROS para subir luego al banco con el detalle de los cobros por PAC
 #   a los socios de FLC inscritos.
 #
-# Estructura ARCHIVO UNIVERSO
+# - Estructura ARCHIVO UNIVERSO
 #
-#   0   (3)      3     (3)      6     (3)       9     10    (22)        32          33   (8)       41     (8)       49
-#   | cod. banco | cod. empresa | cod. convenio | 'D' | id. de servicio | 'espacio' | AAAAMMDD gen | AAAAMMDD serv. | 'espacio' |
+#     0   (3)      3     (3)      6     (3)       9     10    (22)        32          33   (8)       41     (8)       49
+#     | cod. banco | cod. empresa | cod. convenio | 'D' | id. de servicio | 'espacio' | AAAAMMDD gen | AAAAMMDD serv. | 'espacio' |
 #
-#   ...:::...:......................:........::::::::.
-#   001015051D0000000000000029788537 2017103120161109
+#     ...:::...:......................:........::::::::.
+#     001015051D0000000000000029788537 2017103120161109
 #
-# Estructura NOMINA DE COBROS
+# - Estructura NOMINA DE COBROS
 #
-#   0     (3)    3     (3)      6      (3)      9     10     (22)       32          33     (10)      43    (11)    54     (8)       62     (8)       70 (10)
-#   | cod. banco | cod. empresa | cod. convenio | 'D' | id. de servicio | 'espacio' | info adicional | monto cargo | AAAAMMDD fact. | AAAAMMDD venc. | .'s |
+#     0     (3)    3     (3)      6      (3)      9     10     (22)       32          33     (10)      43    (11)    54     (8)       62     (8)       70 (10)
+#     | cod. banco | cod. empresa | cod. convenio | 'D' | id. de servicio | 'espacio' | info adicional | monto cargo | AAAAMMDD fact. | AAAAMMDD venc. | .'s |
 #
 #
-#   ...:::...:......................:..........:::::::::::........::::::::..........
-#   001015051D029788537              L.GALLARDO000002929002017110120171106..........
+#     ...:::...:......................:..........:::::::::::........::::::::..........
+#     001015051D029788537              L.GALLARDO000002929002017110120171106..........
 #
 #
 #######################################################################################################################
 
-# noinspection PyUnresolvedReferences
-import numpy as np
 from xlrd import open_workbook
 
 # 1. Extraer nombres y montos de cobro de 'Registro Donaciones.xls'
 #
 
-mes = "Septiembre 2017"
+mes = 'Octubre 2017'
 wb = open_workbook('Registro Donaciones.xls')
 
 items = []
@@ -52,6 +50,11 @@ for row in range (1,nrows):
     value = sheet.cell(row, 0).value
     if value == mes:
         start_row = row
+        break;
+
+if start_row == 0:
+    print 'Error: no se encontro mes ingresado en registro de donaciones'
+    exit()
 
 # Extraer nombres y montos de cobro
 for row in range (start_row, nrows):
@@ -68,31 +71,39 @@ for row in range (start_row, nrows):
     montos_uf.append(str(int(monto_uf)))
     montos_clp.append(str(int(monto_clp)))
 
-    #print(sheet.cell(0,0).value)
-    #print(start)
-    #print(end)
-    #print(Nombre)
-    print(montos_clp)
-
 
 # 2. Leer archivo universo y escribir archivo de cobros
 #
-file_object = open("Cobro noviembre.txt", "r")
+file_object = open('cobros pasados/Cobro octubre.txt', 'r')
 
-spaces = "              " # 14 espacios
-dots   = ".........."
-contador = 0
+# TODO: como se definen las fechas de facturacion y vencimiento??
+fecha_facturacion = '20171102'
+fecha_vencimiento = '20171106'
+dots   = '..........'
 
 archivo_final = []
 
+i = 0
 for line in file_object:
-    aux1 = line[0:10]
-    aux2 = line[23:32]
-    monto_cobro = (str(montos_clp[contador] + "00")).zfill(11)
-    archivo_final.append(aux1 + aux2 + spaces + nombres[contador] + monto_cobro + dots)
-    contador += 1
+    if line[9] == 'D':
+        aux1 = line[0:10] # cod. banco + cod. empresa + cod. convenio + 'D'
+        aux2 = line[23:32].ljust(23) # id. servicio
+        monto_cobro = str(montos_clp[i] + '00').zfill(11)
+        nombre_socio = nombres[i].ljust(10)
 
-a = "0020"
-F = str(Monto[1]+a)
+        linea_cobro = aux1 + aux2 + nombre_socio + monto_cobro + fecha_facturacion + fecha_vencimiento + dots
+        print str(i+1) + ', ' + linea_cobro
+        archivo_final.append(linea_cobro)
+
+        i += 1
+    elif line[9] == 'T':
+        if int(line[33:39]) != i:
+            print 'Error: numero de socios procesados vs indicados en universo, no coinciden'
+    else:
+        print 'Error: archivo universo corrupto'
+
+
+a = '0020'
+F = str(montos_clp[1]+a)
 #a[-3:-1]=b
 print(F.zfill(13))
